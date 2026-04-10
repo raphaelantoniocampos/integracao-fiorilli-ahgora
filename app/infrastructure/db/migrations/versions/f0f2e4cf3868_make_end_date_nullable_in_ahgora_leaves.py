6cdf6c69a16d080a9991f957af7247dc9544f739
Raphael Campos <raphael.antonio.campos@proton.me>
2026-04-10 12:51:01 -0300

    update html

--- app/infrastructure/db/migrations/versions/f0f2e4cf3868_make_end_date_nullable_in_ahgora_leaves.py
+++ app/infrastructure/db/migrations/versions/f0f2e4cf3868_make_end_date_nullable_in_ahgora_leaves.py
@@ -9,7 +9,6 @@ Create Date: 2026-03-18 16:44:31.496028
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
